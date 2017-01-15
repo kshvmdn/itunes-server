a2d807ade050172331d9855ba0da44ada16fee9c
Kashav Madan <kshvmdn@gmail.com>
2017-01-14 19:24:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+## itunes-server
+
+Control iTunes over a server. Uses [`osascript`](https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/osascript.1.html).
+
+### Installation / setup
+
+  - Use the following to install and run (assuming you have Go [installed](https://golang.org/doc/install) and [configured](https://golang.org/doc/install#testing)):
+
+    ```sh
+    $ go get github.com/kshvmdn/itunes-server
+    $ itunes-server # Listening at localhost:8080
+    ```
+
+  - You can also build directly from source, if you'd prefer that.
+
+    ```sh
+    $ git clone https://github.com/kshvmdn/itunes-server.git
+    $ cd itunes-server
+    $ go build itunes-server.go
+    $ ./itunes-server # Listening at localhost:8080
+    ```
+
+### Usage
+
+  - `itunes-server` provides a list of endpoints which you can access via standard HTTP requests.
+  
+  - Endpoints:
+
+    + __GET /__
+      * View current status and song (if playing).
+    + __GET /open__
+      * Open iTunes.
+    + __GET /exit__
+      * Exit iTunes.
+    + __GET /play__
+      * Play the last-played song (iff nothing is currently playing).
+    + __GET /pause__
+      * Pause the currently playing song.
+    + __GET /stop__
+      * Stop the currently playing song.
+    + __GET /next__
+      * Skip to the next song.
+    + __GET /prev__
+      * Play the previous song.
+    + __GET /mute__
+      * Mute iTunes (doesn't effect system volume).
+    + __GET /unmute__
+      * Unmute iTunes (doesn't effect system volume).
+    + __GET /shuffle__
+      * Play a random song from your iTunes library.
+    + __GET /tracks__
+      * View list of tracks in your iTunes library.
+      * Query parameters:
+        * `limit` - The number of tracks to show (defaults to 100)
+        * `skip` - The number of tracks to skip (defaults to 0)
+    + __GET /play/track/:track_name__
+      * Play track(s) with name that matches `track_name`.
+    + __GET /play/artist/:artist_name__
+      * Play track(s) with artist that matches `artist_name`.
+    + __GET /play/album/:album_name__
+      * Play track(s) with album that matches `album_name`.
+
+  - Examples (these endpoints can be accessed through the browser as well):
+
+    + View current status:
+      
+      ```sh
+      curl -L localhost:8080
+      {"status":"playing","current":{"title":"Gobstopper","artist":"J Dilla","album":"Donuts"}}
+      ```
+
+    + Next:
+
+      ```sh
+      curl -L localhost:8080/next
+      {"status":"playing","current":{"title":"One For Ghost","artist":"J Dilla","album":"Donuts"}}
+      ```
+
+    + Pause:
+
+      ```sh
+      curl -L localhost:8080/pause
+      {"status":"paused","current":{"title":"","artist":"","album":""}}
+      ```
+
+    + Play
+    
+      ```sh
+      curl -L localhost:8080/play
+      {"status":"playing","current":{"title":"One For Ghost","artist":"J Dilla","album":"Donuts"}}
+      ```
+
+  - If you're looking to expose your local server (so people can access iTunes without having to be on the same connection), I suggest using [ngrok](https://ngrok.com/).
+
+### Contribute
+
+This project is completely open source, feel free to open an issue or create a pull request.
---
+++ itunes-server.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"unsafe"
+
+	"github.com/gorilla/mux"
+)
+
+type Track struct {
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Album  string `json:"album"`
+}
+
+type Status struct {
+	Status  string `json:"status"`
+	Current Track  `json:"current"`
+}
+
+type Route struct {
+	Endpoint string
+	Command  string
+}
+
+func ReaderToString(out io.ReadCloser) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(out)
+	b := buf.Bytes()
+	s := *(*string)(unsafe.Pointer(&b))
+	return strings.Trim(s, "\n")
+}
+
+func ExecItunes(cmd string) string {
+	fullCmd := fmt.Sprintf("tell Application \"iTunes\" %s", cmd)
+	c := exec.Command("/usr/bin/osascript", "-e", fullCmd)
+
+	out, _ := c.StdoutPipe()
+
+	defer c.Wait()
+
+	if err := c.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	return ReaderToString(out)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	status := ExecItunes("to player state as string")
+
+	var name string
+	var artist string
+	var album string
+
+	if status == "playing" {
+		name = ExecItunes("to name of current track as string")
+		artist = ExecItunes("to artist of current track as string")
+		album = ExecItunes("to album of current track as string")
+	}
+
+	json.NewEncoder(w).Encode(Status{status, Track{name, artist, album}})
+}
+
+func ListTracks(w http.ResponseWriter, r *http.Request) {
+	var wg sync.WaitGroup
+	var tracks []Track
+
+	start, err := strconv.Atoi(r.FormValue("skip"))
+	if err != nil {
+		start = 0
+	}
+
+	limit, err := strconv.Atoi(r.FormValue("limit"))
+	if err != nil || limit > 100 {
+		limit = 100
+	}
+
+	trackIds := ExecItunes("to get the id of (every track)")
+
+	end := start + limit
+	if end > len(trackIds) {
+		end = len(trackIds) - 1
+	}
+
+	wg.Add(end - start)
+
+	for _, id := range strings.Split(trackIds, ", ")[start:end] {
+		go func(id string) {
+			defer wg.Done()
+			name := ExecItunes(fmt.Sprintf("to get the name of track id %s", id))
+			artist := ExecItunes(fmt.Sprintf("to get the artist of track id %s", id))
+			album := ExecItunes(fmt.Sprintf("to get the album of track id %s", id))
+			tracks = append(tracks, Track{name, artist, album})
+		}(id)
+	}
+
+	wg.Wait()
+	json.NewEncoder(w).Encode(tracks)
+}
+
+func PlaySong(w http.ResponseWriter, r *http.Request) {
+	songName := mux.Vars(r)["song_name"]
+	cmd := fmt.Sprintf("to play (every track of playlist \"Library\" whose name is \"%s\")", songName)
+	ExecItunes(cmd)
+	http.Redirect(w, r, "/", 302)
+}
+
+func PlayArtist(w http.ResponseWriter, r *http.Request) {
+	artistName := mux.Vars(r)["artist_name"]
+	cmd := fmt.Sprintf("to play (every track of playlist \"Library\" whose artist is \"%s\")", artistName)
+	ExecItunes(cmd)
+	http.Redirect(w, r, "/", 302)
+}
+
+func PlayAlbum(w http.ResponseWriter, r *http.Request) {
+	albumName := mux.Vars(r)["album_name"]
+	cmd := fmt.Sprintf("to play (every track of playlist \"Library\" whose album is \"%s\")", albumName)
+	ExecItunes(cmd)
+	http.Redirect(w, r, "/", 302)
+}
+
+func PlayRandom(w http.ResponseWriter, r *http.Request) {
+	trackIds := strings.Split(ExecItunes("to get the id of (every track)"), ", ")
+	cmd := fmt.Sprintf("to play (every track of playlist \"Library\" whose id is \"%s\")", trackIds[rand.Intn(len(trackIds))])
+	ExecItunes(cmd)
+	http.Redirect(w, r, "/", 302)
+}
+
+func Routes() []Route {
+	return []Route{
+		{"open", "to open"},
+		{"exit", "to quit"},
+		{"play", "to play"},
+		{"pause", "to pause"},
+		{"stop", "to stop"},
+		{"next", "to next track"},
+		{"prev", "to previous track"},
+		{"mute", "to set mute to true"},
+		{"unmute", "to set mute to false"},
+	}
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/tracks", ListTracks)
+	router.HandleFunc("/play/track/{track_name}", PlaySong)
+	router.HandleFunc("/play/artist/{artist_name}", PlayArtist)
+	router.HandleFunc("/play/album/{album_name}", PlayAlbum)
+	router.HandleFunc("/shuffle", PlayRandom)
+
+	for _, route := range Routes() {
+		func(endpoint string, cmd string) {
+			router.HandleFunc(fmt.Sprintf("/%s", endpoint), func(w http.ResponseWriter, r *http.Request) {
+				ExecItunes(cmd)
+				http.Redirect(w, r, "/", 302)
+			})
+		}(route.Endpoint, route.Command)
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
